cdc6c6807e2b0d7259a7e4940d6e0e97f76bc05d
Xihao <xihao.zhou@outlook.com>
2025-06-29 20:59:23 +0800

    Added base app structure and logic for question 1

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# xihao-htx-tech-assessment-de
-HTX xCode Technical Test Question – Data Engineer
+# HTX xCode Technical Assessment - Data
+
+## 🧾 Overview
+
+## 🛠 Prerequisites
+
+Ensure you have the following installed:
+
+- Python 3.10+
+- Git
+
+## 📦 Setup
+
+1. **Clone or download the repo as zip file:**
+
+   ```bash
+   git clone https://github.com/xihao-zhou/xihao-htx-tech-assessment-de.git
+   cd xihao-htx-tech-assessment-de
+   ```
+
+2. **Create and activate a virtual environment:**
+
+   **Linux/MacOs**
+   ```bash
+   python3 -m venv .venv
+   source .venv/bin/activate
+
+   deactivate #command to deactivate virtual environment
+   ```
+
+   **Windows**
+   ```bash
+   python -m venv .venv
+   .\.venv\Scripts\activate
+
+   deactivate #command to deactivate virtual environment
+   ```
+   
+3. **Install dependencies:**
+   
+   **Linux/MacOs**
+   ```bash
+   pip3 install -r requirements.txt
+   ```
+
+   **Windows**
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+
+## 🚀 Running the App
+
+   **Linux/MacOs**
+   ```bash
+   python3 app/main.py
+   ```
+   
+   **Windows**
+   ```bash
+   python .\app\main.py
+   ```
+
+## 📁 Project Struture
+
+```plaintext
+xihao-htx-tech-assessment-de/
+├── app/
+│   ├── __init__.py
+│   ├── analysis.py
+│   └── main.py
+├── data/
+│   ├── flightData.csv
+│   └── passengers.csv
+├── requirements.txt
+└── README.md
+```
---
+++ app/main.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+from analysis import count_flights_by_month, most_frequent_flyers, most_visited_countires_without_sg, flights_together
+
+def main():
+    # Set up path
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    flight_csv = os.path.join(base_dir, "data", "flightData.csv")
+    passengers_csv = os.path.join(base_dir, "data", "passengers.csv")
+    
+    # Load data
+    flight_df = pd.read_csv(flight_csv)
+    passengers_df = pd.read_csv(passengers_csv)
+    
+    while True:
+        print("1. Question 1: Total Number of Flights Each Month")
+        print("2. Question 2: Top 10 Most Frequent Flyers")
+        print("3. Question 3: Most Visited Countires Without SG")
+        print("4. Question 4: Pairs Who Flew Together More Than 2 Times")
+        print("5. Question 4 (With Bonus): Pairs Who Flew Together More Than 2 Times in Date Range")
+        print("0. End app.")
+        choice = input("Please enter your choice: ")
+    
+        if choice == "1":
+            print("\nTotal Number of Flights Each Month\n")
+            print(count_flights_by_month(flight_df))
+            print("\n")
+        
+        elif choice == "2":
+            print("\nTop 10 Most Frequent Flyers\n")
+            print(most_frequent_flyers(flight_df, passengers_df))
+            print("\n")
+            
+        elif choice == "3":
+            print("\nMost Visited Countires Without SG\n")
+            print(most_visited_countires_without_sg(flight_df))
+            print("\n")
+            
+        elif choice == "4":
+            print("\nPairs Who Flew Together More Than 2 Times\n")
+            print(flights_together(flight_df))
+            print("\n")
+            
+        elif choice == "5":
+            print("\nPairs Who Flew Together More Than 2 Times in Date Range\n")
+            print(flights_together(flight_df))
+            print("\n")
+            
+        elif choice == "0" or choice == "End" or choice == "end" or choice == "Stop" or choice == "stop":
+            break
+            
+        else:
+            print("\nYou have entered an invalid choice, please choose again.\n")
+    
+if __name__ == "__main__":
+   main()
